7623257d35740103a58914bd4ac401ac11f9575d
aparnaroy <94129173+aparnaroy@users.noreply.github.com>
2023-02-07 13:39:27 -0500

    Added 'Hello World' to App

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App(): JSX.Element {
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Aparna Roy.
+                automatically reload. Aparna Roy. Hello World!
             </p>
         </div>
     );
